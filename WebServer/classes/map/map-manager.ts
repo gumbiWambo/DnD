277f16ebdc59bdf37e1cdae85372dca11b7f0a53
Fabian Gumbrecht <fabiangumbrecht@web.de>
2021-01-09 19:59:05 +0100

    map and fruits

--- WebServer/classes/map/map-manager.ts
+++ WebServer/classes/map/map-manager.ts
@@ -14,7 +14,7 @@ export class MapManager {
   private mapDrawer: MapDrawer;
   constructor(){
     this.mapDrawer = new MapDrawer(this.map);
-    this.mapDrawer.initiate(30, 30);
+    this.mapDrawer.initiate(31, 31);
     this.mapDrawer.drawBorder();
     this.drawContent();
   }
@@ -84,103 +84,7 @@ export class MapManager {
     }
   }
   private drawContent() {
-    this.drawCellEnviornment();
-    this.drawTreasureEnviornment();
-  }
-  private drawTreasureEnviornment() {
-    const treasureChamberDoors: DungonDoor[] = [];
-    treasureChamberDoors.push(this.mapDrawer.setDoor(14, 24, false, false));
-    treasureChamberDoors.push(this.mapDrawer.setDoor(15, 24, false, false));
-    treasureChamberDoors.forEach(door => door.locked = true);
-    const levers: DungonLever[] = [];
-    let leverOrder: number[] = [];
-    const checkOrder = (): boolean => {
-      return leverOrder.length === 6
-        && leverOrder[0] === 5
-        && leverOrder[1] === 3
-        && leverOrder[2] === 1
-        && leverOrder[3] === 4
-        && leverOrder[4] === 6
-        && leverOrder[5] === 2
-    }
-    const pushNumber = (leverNumber: number): boolean => {
-      leverOrder.push(leverNumber);
-      if(leverOrder.length === 6) {
-        if(checkOrder()) {
-          treasureChamberDoors.forEach(door => {
-            door.locked = false;
-            if(!door.passableTarain) {
-              door.interact();
-            }
-          })
-        } else {
-          leverOrder = [];
-          let counter = 0;
-          const interval = setInterval(() => {
-            if(counter % 2 === 0) {
-              treasureChamberDoors.forEach(x => x.locked = true);
-            } else {
-              treasureChamberDoors.forEach(x => x.locked = false);
-            }
-            this.sendMap();
-            ++counter;
-            if(counter > 10) {
-              clearInterval(interval);
-            }
-          }, 300)
-        }
-      }
-      return true;
-    }
-    const lever1 = this.mapDrawer.setLever(13, 23, () => pushNumber(1), () => pushNumber(1));
-    const lever2 = this.mapDrawer.setLever(11, 23, () => pushNumber(2), () => pushNumber(2));
-    const lever3 = this.mapDrawer.setLever(18, 23, () => pushNumber(3), () => pushNumber(3));
-    const lever4 = this.mapDrawer.setLever(12, 23, () => pushNumber(4), () => pushNumber(4));
-    const lever5 = this.mapDrawer.setLever(17, 23, () => pushNumber(5), () => pushNumber(5));
-    const lever6 = this.mapDrawer.setLever(16, 23, () => pushNumber(6), () => pushNumber(6));
-  }
-  private drawCellEnviornment() {
-    this.drawCellLeft();
-    this.drawCellLeft(0, 7);
-    this.drawCellLeft(0, 14);
-    this.drawCellRight()
-    this.drawCellRight(0, 7);
-    this.drawCellRight(0, 14);
-    this.mapDrawer.drawHorizontalWall(7, 22, 24);
-    this.mapDrawer.setCampFire(3, 7);
-    this.mapDrawer.setCampFire(3, 14);
-    this.mapDrawer.setCampFire(3, 21);
-    this.mapDrawer.setCampFire(26, 7);
-    this.mapDrawer.setCampFire(26, 14);
-    this.mapDrawer.setCampFire(26, 21);
-    
-    const cellDoors: DungonDoor[] = [];
-    cellDoors.push(this.mapDrawer.setDoor(6, 7, false, true));
-    cellDoors.push(this.mapDrawer.setDoor(6, 14, false, true));
-    cellDoors.push(this.mapDrawer.setDoor(6, 21, false, true));
-    cellDoors.push(this.mapDrawer.setDoor(23, 7, false, true));
-    cellDoors.push(this.mapDrawer.setDoor(23, 14, false, true));
-    cellDoors.push(this.mapDrawer.setDoor(23, 21, false, true));
-    cellDoors.forEach(door => door.locked = true);
 
-    const action = (x: any): boolean => {
-      cellDoors.forEach(door => {
-        door.locked = false;
-      });
-      this.mapDrawer.drawHorizontalWall(7, 22, 4);
-      return true;
-    }
-    const reverse = (x: any): boolean => {
-      cellDoors.forEach(door => {
-        if(door.passableTarain) {
-          door.interact();
-        }
-        door.locked = true;
-      });
-      this.mapDrawer.drawHorizontalGround(7, 22, 4);
-      return true;
-    }
-    this.mapDrawer.setLever(14, 2, action, reverse);
   }
   private drawCellLeft(offsetX: number = 0, offsetY: number = 0) {
     this.mapDrawer.drawHorizontalWall(1 + offsetX, 6 + offsetX, 4 + offsetY);
