bb6ad76db1403794d5f322bd34add24d9eadeec0
Fabian Gumbrecht <fabiangumbrecht@web.de>
2021-03-03 21:07:03 +0100

    fruits

--- WebServer/classes/map/map-manager.ts
+++ WebServer/classes/map/map-manager.ts
@@ -14,24 +14,8 @@ export class MapManager {
   private connections: MapConnection[] = [];
   private creatureCoordinates: PlayerCoordinate[] = [];
   constructor() {
-    this.maps.push(new Map('main'));
-    this.maps.push(new Map('Left1'));
-    this.maps.push(new Map('Left2'));
-    this.maps.push(new Map('Left3'));
-    this.maps.push(new Map('Right1'));
-    this.maps.push(new Map('Right2'));
-    this.maps.push(new Map('Right3'));
-    this.maps.push(new Map('Floor'));
-    this.mapTransitions.push(new MapTransition('main', 9, 3,'Left1', 0, 3));
-    this.mapTransitions.push(new MapTransition('main', 9, 8,'Left2', 0, 3));
-    this.mapTransitions.push(new MapTransition('main', 9, 12,'Left3', 0, 3));
-    this.mapTransitions.push(new MapTransition('main', 0, 3,'Right1', 9, 3));
-    this.mapTransitions.push(new MapTransition('main', 0, 8,'Right2', 9, 3));
-    this.mapTransitions.push(new MapTransition('main', 0, 12,'Right3', 9, 3));
-    this.mapTransitions.push(new MapTransition('main', 4, 16,'Floor', 17, 0));
-    this.mapTransitions.push(new MapTransition('main', 5, 16,'Floor', 18, 0));
-
-    // this.maps[0].drawer.drawBorder();
+    this.maps.push(new Map('mainHall'));
+    this.maps.push(new Map('mainRing'));
     this.drawContent();
     this.subscribeTransitions();
   }
@@ -152,119 +136,12 @@ export class MapManager {
   }
   private drawContent() {
     this.drawMainMap();
-    this.drawFloor();
-    this.drawLeft();
-    this.drawRight();
   }
   //#region DrawMaps
   private drawMainMap() {
     const mainMap = this.maps[0];
     mainMap.drawer.initiate(17, 10);
     mainMap.drawer.drawBorder();
-    mainMap.drawer.setDoor(4,0, false, false);
-    mainMap.drawer.setDoor(5,0, false, false);
-    mainMap.drawer.setDoor(4, 16, false, false).locked = true;
-    mainMap.drawer.setDoor(5, 16, false, false).locked = true;
-    mainMap.drawer.setDoor(9, 3, false, true);
-    mainMap.drawer.setDoor(9, 8, false, true);
-    mainMap.drawer.setDoor(9, 12, false, true);
-    mainMap.drawer.setDoor(0, 3, false, true);
-    mainMap.drawer.setDoor(0, 8, false, true);
-    mainMap.drawer.setDoor(0, 12, false, true);
-  }
-  private drawLeft() {
-    this.drawLeft1();
-    this.drawLeft2();
-    this.drawLeft3();
-  }
-  private drawRight() {
-    this.drawRight1();
-    this.drawRight2();
-    this.drawRight3();
-  }
-  private drawLeft1() {
-    const left1 = this.maps[1];
-    left1.drawer.initiate(10, 10);
-    left1.drawer.drawBorder();
-    left1.drawer.setDoor(0, 3, false, true);
-    let openTheDoors = false;
-    const leverReverse = (x: any) => {
-      this.maps[0].drawer.drawHorizontalGround(1, 8, 14);
-      this.maps[5].drawer.drawHorizontalGround(4, 4, 5);
-      return true;
-    }
-    const leverAction = (x: any) => {
-      this.maps[0].drawer.drawHorizontalWall(1, 8, 14);
-      const lever2Action = (x: any) => {
-        const lever3Action = (x: any) => {
-          if(!!openTheDoors) {
-            this.maps[0].fields[16][4].locked = false;
-            this.maps[0].fields[16][5].locked = false;
-          } else {
-            openTheDoors = true
-          }
-          return true;
-        }
-        const lever3Reverse = (x: any) => {
-          if(!!openTheDoors && !this.maps[0].fields[16][4].locked) {
-            this.maps[0].fields[16][4].locked = true;
-            this.maps[0].fields[16][5].locked = true;
-          } else {
-            openTheDoors = false;
-          }
-          return true;
-        }
-        this.maps[4].drawer.setLever(5, 4, lever3Action, lever3Reverse);
-        this.maps[6].drawer.setLever(5, 4, lever3Action, lever3Reverse);
-        return true;
-      }
-      const lever2Reverse = (x: any) => {
-        return true;
-      }
-      this.maps[5].drawer.setLever(4, 5, lever2Action, lever2Reverse)
-      return true;
-    }
-
-    left1.drawer.setLever(5, 4, leverAction, leverReverse);
-  }
-  private drawLeft2() {
-    const left1 = this.maps[2];
-    left1.drawer.initiate(10, 10);
-    left1.drawer.drawBorder();
-    left1.drawer.setDoor(0, 3, false, true);
-
-  }
-  private drawLeft3() {
-    const left1 = this.maps[3];
-    left1.drawer.initiate(10, 10);
-    left1.drawer.drawBorder();
-    left1.drawer.setDoor(0, 3, false, true);
-
-  }
-  private drawRight1() {
-    const right1 = this.maps[4];
-    right1.drawer.initiate(10, 10);
-    right1.drawer.drawBorder();
-    right1.drawer.setDoor(9, 3, false, true);
-  }
-  private drawRight2() {
-    const right2 = this.maps[5];
-    right2.drawer.initiate(10, 10);
-    right2.drawer.drawBorder();
-    right2.drawer.setDoor(9, 3, false, true);
-  }
-  private drawRight3() {
-    const right3 = this.maps[6];
-    right3.drawer.initiate(10, 10);
-    right3.drawer.drawBorder();
-    right3.drawer.setDoor(9, 3, false, true);
-  }
-  private drawFloor() {
-    const floor = this.maps[7];
-    floor.drawer.initiate(30, 30);
-    floor.drawer.drawBorder();
-    floor.drawer.setDoor(18, 0, false, false)
-    floor.drawer.setDoor(17, 0, false, false)
   }
   //#endregion DrawMaps
   private sendCreatureCoordinates(map: Map) {
